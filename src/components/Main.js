b42b97b5131cba3b0b409889a28add6a407e9cec
Ilaria Callegari <ilaria.callegari@gmail.com>
2021-09-27 19:08:05 +0100

    create header main design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,34 @@
 import useStyles from "../styles/main-style";
 import Navbar from "./Navbar";
+import iconPlus from "../assets/icon-plus.svg";
+import iconArrowDown from "../assets/icon-arrow-down.svg";
 
 function Main() {
-    const {mainCtn} = useStyles()
+  const { mainCtn, mainSection, header, headerRight, filter, createButton } =
+    useStyles();
   return (
     <div className={mainCtn}>
       <Navbar />
-      <main>main</main>
+      <main className={mainSection}>
+        <div className={header}>
+          <div>
+            <h1>Invoices</h1>
+            <p>There are 7 total invoices</p>
+          </div>
+          <div className={headerRight}>
+            <div className={filter}>
+              <p>Filter by status</p>
+              <img src={iconArrowDown} alt="arrow down" />
+            </div>
+            <div className={createButton}>
+              <div>
+                <img src={iconPlus} alt="plus sign" />
+              </div>
+              <p>New Invoice</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/styles/main-style.js
+++ src/styles/main-style.js
@@ -6,6 +6,56 @@ const useStyles = createUseStyles({
     display: "flex",
     background: colors.white,
   },
+  mainSection: {
+    width: "70%",
+    margin: "7% auto",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    "& div h1": {
+      fontSize: "2rem",
+      fontWeight: 300,
+      letterSpacing: "1px",
+      marginBottom: "1rem",
+    },
+    "& div:nth-child(1) p": {
+      color: colors.grey,
+    },
+  },
+  headerRight: {
+    display: "flex",
+    width: "30%",
+    justifyContent: "space-between"
+  },
+  filter: {
+    display: "flex",
+    alignItems: "center",
+    "& img": {
+        marginLeft: "2rem"
+    }
+  },
+  createButton: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    background: colors.darkPurple,
+    color: "#fff",
+    padding: "0.8rem 1rem",
+    borderRadius: "2rem",
+
+    "& div": {
+        background: "#fff",
+        width: "2.2rem",
+        height: "2.2rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "50%",
+        marginRight: "1rem"
+    }
+  },
 });
 
 export default useStyles;
